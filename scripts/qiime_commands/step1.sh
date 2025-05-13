bef91accd824237d8e68ca1825747031a532ef12
jippeS <jippedominicussilvius@gmail.com>
2025-05-13 00:18:38 +0000

    add qiime commands

--- scripts/qiime_commands/step1.sh
+++ scripts/qiime_commands/step1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-folder_path = $1
-metadata = $2
-forward_primer_seq = $3
-reverse_primer_seq = $4
-name_convention_PairEndSequences = $5
+folder_path = "$1"
+metadata = "$2"
+forward_primer_seq = "$3"
+reverse_primer_seq = "$4"
+name_convention_PairEndSequences = "$5"
 
 python3 wetsus_repo_analysis/scripts/pre_data.py "$folder_path/"
 mkdir -p "$folder_path/input/raw_data"
