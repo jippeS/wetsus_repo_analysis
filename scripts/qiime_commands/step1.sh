019fd7ca9db29c12b00c110eae67abb5091aee74
jippeS <jippedominicussilvius@gmail.com>
2025-05-13 16:11:28 +0000

    add qiime commands

--- scripts/qiime_commands/step1.sh
+++ scripts/qiime_commands/step1.sh
@@ -6,6 +6,7 @@
 #reverse_primer_seq = $4
 #name_convention_PairEndSequences = $5
 #folder_name = $6
+#outputdir = $7
 
 python3 wetsus_repo_analysis/scripts/pre_data.py "$1/"
 mkdir -p "$1/input/raw_data"
@@ -15,5 +16,5 @@ mkdir "$1/output/"
 python3 wetsus_repo_analysis/scripts/pre_demux.py --inputdir="$1/" --forward="$3" --reverse="$4"
 gzip -c "$1/input/raw_data/forward.fastq" > "$1/input/raw_data/forward.fastq.gz"
 gzip -c "$1/input/raw_data/reverse.fastq" > "$1/input/raw_data/reverse.fastq.gz"
-rm "$1/input/raw_data/*.fastq"
-qiime tools import --type MultiplexedPairedEndBarcodeInSequence --input-path "$1/$6/input/raw_data/" --output-path "$outputdir/$5"
+rm "$1"/input/raw_data/*.fastq
+qiime tools import --type MultiplexedPairedEndBarcodeInSequence --input-path "$1/$6/input/raw_data/" --output-path "$7/$5"
