8c368a2e47135d9a405f687f306801e7aef8b469
jippeS <jippedominicussilvius@gmail.com>
2025-05-13 15:36:52 +0000

    add qiime commands

--- scripts/qiime_commands/step1.sh
+++ scripts/qiime_commands/step1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-folder_path = $1
-metadata = $2
-forward_primer_seq = $3
-reverse_primer_seq = $4
-name_convention_PairEndSequences = $5
+#folder_path = $1
+#metadata = $2
+#forward_primer_seq = $3
+#reverse_primer_seq = $4
+#name_convention_PairEndSequences = $5
 
-python3 wetsus_repo_analysis/scripts/pre_data.py "$folder_path/"
-mkdir -p "$folder_path/input/raw_data"
-mv "$folder_path/input/"*.fastq "$folder_path/input/raw_data/"
-mv "$folder_path/input/"*.txt "$folder_path/input/$metadata"
-mkdir "$folder_path/output/"
-python3 wetsus_repo_analysis/scripts/pre_demux.py --inputdir="$folder_path/" --forward="$forward_primer_seq" --reverse="$reverse_primer_seq"
-gzip -c "$folder_path/input/raw_data/forward.fastq" > "$folder_path/input/raw_data/forward.fastq.gz"
-gzip -c "$folder_path/input/raw_data/reverse.fastq" > "$folder_path/input/raw_data/reverse.fastq.gz"
-rm "$folder_path/input/raw_data/*.fastq"
-qiime tools import --type MultiplexedPairedEndBarcodeInSequence --input-path "$folder_path/$folder_name/input/raw_data/" --output-path "$outputdir/$name_convention_PairEndSequences"
+python3 wetsus_repo_analysis/scripts/pre_data.py "$1/"
+mkdir -p "$1/input/raw_data"
+mv "$1/input/"*.fastq "$1/input/raw_data/"
+mv "$1/input/"*.txt "$1/input/$2"
+mkdir "$1/output/"
+python3 wetsus_repo_analysis/scripts/pre_demux.py --inputdir="$1/" --forward="$3" --reverse="$4"
+gzip -c "$1/input/raw_data/forward.fastq" > "$1/input/raw_data/forward.fastq.gz"
+gzip -c "$1/input/raw_data/reverse.fastq" > "$1/input/raw_data/reverse.fastq.gz"
+rm "$1/input/raw_data/*.fastq"
+qiime tools import --type MultiplexedPairedEndBarcodeInSequence --input-path "$1/$folder_name/input/raw_data/" --output-path "$outputdir/$5"
