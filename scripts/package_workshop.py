af9c1ed7c42e2808bd5c6462ae7c9c99c3cc7fe0
jippeS <jippedominicussilvius@gmail.com>
2025-06-10 14:27:21 +0000

    changes

--- scripts/package_workshop.py
+++ scripts/package_workshop.py
@@ -71,7 +71,9 @@ def upload_files(files, shutil, folder_path):
 
 def naming_convention(folder_path, widgets):
     outputdir = folder_path + "/output"
-    cores = 2
+
+    num_cores = os.cpu_count()
+    cores =  int(os.cpu_count())
 
     # Create an "input bar" at the top
     input_box = widgets.Text(
@@ -95,6 +97,7 @@ def naming_convention(folder_path, widgets):
     # Display it nicely at the top
     display(input_box, output_box)
 
+    print(f"Number of CPU cores: {num_cores}")
     return input_box, cores, outputdir
 
 def create_names(input_box):
@@ -291,9 +294,3 @@ def qiime_arguments():
     p_trunc_len_f = 200
     p_trunc_len_r = 190
     return error_rate, p_trim_left, p_trim_right, p_trunc_len_f, p_trunc_len_r
-
-def get_cores():
-    num_cores = os.cpu_count()
-    cores =  int(os.cpu_count())
-    print(f"Number of CPU cores: {num_cores}")
-    return cores
